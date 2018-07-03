ff498c448bcc5646da7f432a1a768bfd7f40ec11
diegoveloper <diego.velasquez.lopez@gmail.com>
2018-07-03 13:45:43 -0500

    Splash Screen

--- android/app/src/main/java/com/example/fluttersamples/MainActivity.java
+++ android/app/src/main/java/com/example/fluttersamples/MainActivity.java
@@ -4,10 +4,28 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
+import android.view.ViewTreeObserver;
+import android.view.WindowManager;
+
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    //make transparent status bar
+    getWindow().setStatusBarColor(0x00000000);
+    
     GeneratedPluginRegistrant.registerWith(this);
+    
+    //Remove full screen flag after load
+    ViewTreeObserver vto = getFlutterView().getViewTreeObserver();
+    vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+      @Override
+      public void onGlobalLayout() {
+        // do something now when the object is loaded
+        // e.g. find the real size of it etc
+        getFlutterView().getViewTreeObserver().removeOnGlobalLayoutListener(this);
+        getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+      }
+    });
   }
 }
--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -5,6 +5,7 @@
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  UIApplication.sharedApplication.statusBarHidden = false;
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
